50f57585a605748cfa7bd2dec7baddf0a89669ea
uuboyscy <aegis12321@gmail.com>
2024-02-17 16:36:24 +0800

    refactor: :recycle: post create command

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,6 +2,13 @@
 set -x
 
 cp /root/.gitconfig_mounted /root/.gitconfig \
-&& cp -rp /root/.ssh_mounted/* /root/.ssh/ \
-&& git config --global --add safe.directory /workspaces/demo-devcontainer \
-&& zsh
+&& cp -r /root/.ssh_mounted/* /root/.ssh/ \
+&& chmod 400 /root/.ssh/id_rsa \
+&& chmod 755 /root/.ssh/config
+
+git config --global --add safe.directory /workspaces/swag
+
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="xiong-chiamiov-plus"/' /root/.zshrc \
+&& sed -i '/HIST_STAMPS="mm\/dd\/yyyy"/s/^#*//;s/mm\/dd\/yyyy/yyyy-mm-dd/' /root/.zshrc
+
+zsh
