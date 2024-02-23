f86245b9091739a11e790229026534d42a63a875
uuboyscy <aegis12321@gmail.com>
2024-02-23 11:42:31 +0800

    fix: git safe directory

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,7 +6,7 @@ cp /root/.gitconfig_mounted /root/.gitconfig \
 && chmod 400 /root/.ssh/id_rsa \
 && chmod 755 /root/.ssh/config
 
-git config --global --add safe.directory /workspaces/swag
+git config --global --add safe.directory /workspaces/*
 
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="xiong-chiamiov-plus"/' /root/.zshrc \
 && sed -i '/HIST_STAMPS="mm\/dd\/yyyy"/s/^#*//;s/mm\/dd\/yyyy/yyyy-mm-dd/' /root/.zshrc
