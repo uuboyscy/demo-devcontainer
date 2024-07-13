1b507e2eeab10c84ffbf0fa0a4895bb4754a2f90
uuboyscy <aegis12321@gmail.com>
2024-07-13 20:41:14 +0800

    build: Commented some codes

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -9,10 +9,10 @@ pip install pipenv \
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="xiong-chiamiov-plus"/' /root/.zshrc \
 && sed -i '/HIST_STAMPS="mm\/dd\/yyyy"/s/^#*//;s/mm\/dd\/yyyy/yyyy-mm-dd/' /root/.zshrc
 
-cp /root/.gitconfig_mounted /root/.gitconfig \
-&& cp -r /root/.ssh_mounted/* /root/.ssh/ \
-&& chmod 400 /root/.ssh/id_rsa \
-&& chmod 755 /root/.ssh/config
+# cp /root/.gitconfig_mounted /root/.gitconfig \
+# && cp -r /root/.ssh_mounted/* /root/.ssh/ \
+# && chmod 400 /root/.ssh/id_rsa \
+# && chmod 755 /root/.ssh/config
 
 git config --global --add safe.directory /workspaces/*
 
